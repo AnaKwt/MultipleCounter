bc24abfec58ee60e87819b64d42a38030d3307a4
Ana <sanotuki.supisn@gmail.com>
2022-06-30 12:19:38 +0900

    Add title list

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
   import { counters } from './stores';
   import CounterList from './CounterList.svelte';
+  
+  let titleListResult: string;
+  function titleList($counters) {
+    titleListResult = "";
+    for(let counter of $counters){
+      titleListResult += titleListResult && counter.title ? `, ${counter.title}`: counter.title; 
+    }
+    return titleListResult;
+  }
 </script>
 
 <h1>Multiple counter</h1>
 
 <CounterList />
+
+<div>
+  title list: {titleList($counters)}
+</div>
