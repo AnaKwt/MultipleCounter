ebfab3c4187f0910d9142d7026aae7799eb26850
Ana <sanotuki.supisn@gmail.com>
2022-06-30 12:17:28 +0900

    Add counter list

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import { counters } from './stores';
+  import CounterList from './CounterList.svelte';
 </script>
 
 <h1>Multiple counter</h1>
+
+<CounterList />
---
+++ src/CounterList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { counters } from './stores';
+
+  function removeCounter(deleteCounter: {count:number, title:string}) {
+    $counters = $counters.filter(counter => counter !== deleteCounter)
+  }
+  
+  function addCounter(){
+    $counters = $counters.concat({count: 0, title: "new"});
+  }
+</script>
+
+{#each $counters as counter}
+  <div>
+    <button on:click={() => removeCounter(counter)}>x</button>
+    <button on:click={() => counter.count -= 1}>-</button>
+    <span>{ counter.count }</span>
+    <button on:click={() => counter.count += 1}>+</button>
+    <button on:click={() => counter.count = 0}>Reset</button>
+    <input
+			bind:value={counter.title}
+		>
+  </div>
+{/each}
+<button on:click={addCounter}>add new counter</button>
